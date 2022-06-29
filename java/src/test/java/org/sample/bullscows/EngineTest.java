a660c67d5f4bfe44c64326afae820861f5b83f37
Mykola Kolisnyk <mkolisnyk@qmetric.co.uk>
2022-06-29 14:45:29 +0100

    Missing dependencies were added

--- java/src/test/java/org/sample/bullscows/EngineTest.java
+++ java/src/test/java/org/sample/bullscows/EngineTest.java
@@ -1,4 +1,11 @@
 package org.sample.bullscows;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class EngineTest {
+	@Test
+	public void testSample() {
+		Assert.assertTrue(true);
+	}
 }
